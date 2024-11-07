06efa5d3c7b3e1a163b8be8f163afa646d5dd46f
Bill Hedworth <bill.hedworth@gmail.com>
2024-11-07 15:21:09 +0000

    first

---
+++ build.clj
@@ -0,0 +1,46 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'org.clojars.bigsy/clj-dynamo-helpers)
+(def version "0.0.1")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+      (b/delete {:path "target"}))
+
+(defn uber [_]
+      (clean nil)
+      (b/write-pom {:class-dir class-dir
+                    :lib lib
+                    :version version
+                    :basis basis
+                    :src-dirs ["src"]
+                    :scm {:url "https://github.com/bigsy/clj-dynamo-helpers"}
+                    :pom-data  [[:licenses
+                                 [:license
+                                  [:name "Eclipse Public License 1.0"]
+                                  [:url "https://opensource.org/license/epl-1-0/"]]]]})
+      (b/copy-dir {:src-dirs ["src" "resources"]
+                   :target-dir class-dir})
+      (b/compile-clj {:basis basis
+                      :src-dirs ["src"]
+                      :class-dir class-dir})
+      (b/uber {:class-dir class-dir
+               :uber-file uber-file
+               :basis basis
+               :main 'clj-dynamo-helpers.core}))
+
+(defn install [_]
+      (b/install {:basis      basis
+                  :lib        lib
+                  :version    version
+                  :jar-file   jar-file
+                  :class-dir  class-dir}))
+
+(defn deploy [_]
+      (dd/deploy {:installer :remote
+                  :artifact uber-file
+                  :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
